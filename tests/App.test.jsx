945c5f46665fe2f74f1864743f80462bba4937ff
Matt <50344355+mkrell23@users.noreply.github.com>
2025-09-17 11:47:55 -0400

    Final library added with actual tests run

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -2,20 +2,9 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../src/App";
 
-describe('something truthy and falsy', () => {
-    it('true to be true', () => {
-        expect(true).toBe(true);
-    });
-
-    it('false to be false', () => {
-        expect(false).toBe(false);
-    });
-});
-
-describe('App', () => {
-    it('renders headline', () => {
-        render(<App title="React" />);
-
-        screen.debug();
+describe("App component", () => {
+    it("Renders correct heading", () => {
+        render(<App/>);
+        expect(screen.getByRole("heading", { level: 1 }).textContent).toMatch(/dog memory game/i)
     })
 });
